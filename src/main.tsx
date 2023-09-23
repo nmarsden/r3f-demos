903d7be6127de9a7486371a9dfe5d5edc661731c
Neil Marsden <neil.marsden@oracle.com>
2023-09-23 17:08:11 +1000

    setup React Three Fiber (r3f) with TheatreJS

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import { getProject } from '@theatre/core'
+import studio from '@theatre/studio'
+import extension from '@theatre/r3f/dist/extension'
+import {editable as e, PerspectiveCamera, SheetProvider} from '@theatre/r3f'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+studio.initialize()
+studio.extend(extension)
+
+const demoSheet = getProject('Demo Project').sheet('Demo Sheet')
+
+const App = () => {
+  return (
+    <Canvas>
+      <SheetProvider sheet={demoSheet}>
+        <PerspectiveCamera theatreKey="Camera" makeDefault position={[-5, 5, 5]} fov={75} lookAt={[0, 0, 0]} />
+        <ambientLight />
+        <e.pointLight theatreKey="Light" position={[10, 10, 5]} />
+        <e.mesh theatreKey="Cube">
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="orange" />
+        </e.mesh>
+      </SheetProvider>
+    </Canvas>
+  )
+}
+
+createRoot(document.getElementById('root')!).render(<App />)
