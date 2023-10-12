091c2535ff0d148975bf2d0944ed1126492d0ccb
Neil Marsden <neil.marsden@oracle.com>
2023-10-12 20:15:19 +1100

    - show a loader to hide the initial render shift

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,19 @@ import './index.css'
 import {createRoot} from 'react-dom/client'
 import {Canvas} from '@react-three/fiber'
 import {Shapes} from "./components/shapes";
+import {Loader} from "@react-three/drei";
 
 const App = () => {
   return (
-    <Canvas
-      shadows={true}
-      camera={{ position: [0, 2, 4.5], fov: 70 }}
-    >
-      <Shapes />
-    </Canvas>
+    <>
+      <Canvas
+        shadows={true}
+        camera={{ position: [0, 2, 4.5], fov: 70 }}
+      >
+        <Shapes />
+      </Canvas>
+      <Loader />
+    </>
   )
 }
 
