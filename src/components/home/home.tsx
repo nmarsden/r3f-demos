62a2b1a8a8ef075ce5f4f8e6f23434e5db4dab71
Neil Marsden <neil.marsden@oracle.com>
2023-11-27 15:34:42 +1100

    - home: improvements to DemoHeading

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import {SpringValue} from "@react-spring/three";
 import {Suspense} from "react";
-import {CuboidCollider, Physics, RigidBody} from "@react-three/rapier";
+import {Physics} from "@react-three/rapier";
 import {DemoBoxes} from "./demoBoxes.tsx";
 import {DemoHeading} from "./demoHeading.tsx";
 
@@ -10,22 +10,8 @@ const Home = ({ opacity }: { opacity: SpringValue }) => {
     <>
       <Suspense>
         <Physics debug={false}>
-          {opacity.isAnimating ? (
-            <>
-              <DemoHeading opacity={opacity}/>
-              <DemoBoxes opacity={opacity} />
-            </>
-          ) : (
-            <>
-              <RigidBody type={'fixed'} colliders={'cuboid'}>
-                <DemoHeading opacity={opacity}/>
-              </RigidBody>
-
-              <DemoBoxes opacity={opacity} />
-
-              <CuboidCollider position={[0, -1.5, 0]} args={[20, 0.2, 20]} />
-            </>
-          )}
+          <DemoHeading opacity={opacity}/>
+          <DemoBoxes opacity={opacity} />
         </Physics>
       </Suspense>
     </>
