453571822163910aa8ea61c18d7c642c5d11833e
Neil Marsden <neil.marsden@oracle.com>
2023-12-15 21:04:59 +1100

    - runner: obstacle now re-positions itself

--- src/components/runner/obstacles.tsx
+++ src/components/runner/obstacles.tsx
@@ -2,41 +2,83 @@
 import {SpringValue, animated, useSpring, config} from "@react-spring/three";
 import {RigidBody} from "@react-three/rapier";
 import * as THREE from "three";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import {useFrame} from "@react-three/fiber";
 
 type ObstaclesProps = {
   opacity: SpringValue;
-  position: THREE.Vector3;
+  groundBounds: THREE.Box2;
 };
 
 const BOX_WIDTH = 0.25;
 const BOX_HEIGHT = 4;
 const BOX_DEPTH = 4;
 const BOX_COLOR = 'red';
+const OBSTACLE_GAP = 10;
 
-const Obstacles = ({ opacity, position }: ObstaclesProps) => {
+const Obstacles = ({ opacity, groundBounds }: ObstaclesProps) => {
+  const [position, setPosition] = useState<THREE.Vector3>(null!);
+  const [nextPosition, setNextPosition] = useState<THREE.Vector3>(null!);
   const [{ obstacleOpacity }, api] = useSpring(() => ({
     from: { obstacleOpacity: 0 },
-    config: config.molasses
+    config: config.stiff
   }))
+  const [updateNextPosition, setUpdateNextPosition] = useState(false);
 
+  // -- Set initial position
   useEffect(() => {
-    if (!opacity.isAnimating) {
-      api.start({
-        to: { obstacleOpacity: 1 }
-      })
+    if (opacity.isAnimating || position || groundBounds.isEmpty()) return;
+
+    setPosition(new THREE.Vector3(groundBounds.max.x + 2, groundBounds.max.y, 0));
+    api.start({
+      delay: 500,
+      from: { obstacleOpacity: 0 },
+      to: { obstacleOpacity: 1 } }
+    );
+    setUpdateNextPosition(true);
+
+  }, [opacity.isAnimating, position, groundBounds]);
+
+  // -- Update position
+  useEffect(() => {
+    if (opacity.isAnimating || updateNextPosition || !nextPosition) return;
+
+    api.start({
+      from: { obstacleOpacity: 1 },
+      to: { obstacleOpacity: 0 },
+      onRest: () => {
+        setPosition(nextPosition.clone());
+        api.start({
+          from: { obstacleOpacity: 0 },
+          to: { obstacleOpacity: 1 }
+        })
+        setUpdateNextPosition(true);
+      }
+    })
+
+  }, [opacity.isAnimating, updateNextPosition, nextPosition])
+
+  // -- Update next position
+  useFrame((state) => {
+    if (opacity.isAnimating || !updateNextPosition) return;
+
+    if ((state.camera.position.x - position.x) > OBSTACLE_GAP) {
+
+      setUpdateNextPosition(false);
+      setNextPosition(new THREE.Vector3(groundBounds.max.x + 2, groundBounds.max.y, 0));
     }
-  }, [opacity.isAnimating]);
+  });
 
   return (
-      (opacity.isAnimating) ? (
+      (opacity.isAnimating || !position) ? (
         <></>
       ) : (
         <>
           <RigidBody
             type={'kinematicPosition'}
+            position={position}
           >
-            <mesh castShadow={true} receiveShadow={true} position={position}>
+            <mesh castShadow={true} receiveShadow={true}>
               <boxGeometry
                 args={[BOX_WIDTH, BOX_HEIGHT, BOX_DEPTH]}
               />
--- src/components/runner/runner.tsx
+++ src/components/runner/runner.tsx
@@ -9,11 +9,7 @@ import {useTransitionState} from "../../hooks/transitionState.ts";
 import {ControlPanel} from "./controlPanel.tsx";
 import {Obstacles} from "./obstacles.tsx";
 import * as THREE from "three";
-import {useFrame} from "@react-three/fiber";
 
-const OBSTACLE_GAP = 10;
-
-// TODO obstacle: refactor to handle respawning itself
 // TODO add a start state
 // TODO add a loss state
 // TODO add a completed state
@@ -22,9 +18,7 @@ const Runner = ({ opacity }: { opacity: SpringValue }) => {
   const transitionState = useTransitionState(opacity);
   const ball = useRef<BallRef>(null);
   const [jumping, setJumping] = useState(false);
-  const [obstaclePosition, setObstaclePosition] = useState(new THREE.Vector3());
-  const [showObstacle, setShowObstacle] = useState(false);
-  const [nextObstaclePosition, setNextObstaclePosition] = useState(new THREE.Vector3());
+  const [groundBounds, setGroundBounds] = useState<THREE.Box2>(new THREE.Box2());
 
   const onButtonClicked = useCallback(() => {
     if (!ball.current || jumping) return;
@@ -40,7 +34,7 @@ const Runner = ({ opacity }: { opacity: SpringValue }) => {
   }, [ball,jumping]);
 
   const onGroundBoundsChanged = useCallback((event: GroundBoundsChangedEvent) => {
-    setNextObstaclePosition(new THREE.Vector3(event.bounds.max.x + 2, event.bounds.max.y, 0));
+    setGroundBounds(event.bounds);
   }, []);
 
   useEffect(() => {
@@ -53,29 +47,13 @@ const Runner = ({ opacity }: { opacity: SpringValue }) => {
     }
   }, [mainContext, transitionState]);
 
-  useEffect(() => {
-    if (opacity.isAnimating || !ball.current) return;
-
-    setShowObstacle(false);
-    setTimeout(() => setShowObstacle(true), 1000);
-  }, [obstaclePosition])
-
-  useFrame((state) => {
-    if (opacity.isAnimating || !ball.current) return;
-
-    // -- Reposition obstacle
-    if ((state.camera.position.x - obstaclePosition.x) > OBSTACLE_GAP) {
-      setObstaclePosition(nextObstaclePosition.clone());
-    }
-  });
-
   return (
     <>
       <Suspense>
         <Physics debug={false}>
           <Ball ref={ball} opacity={opacity}/>
           <Ground opacity={opacity} onGroundHit={onGroundHit} onGroundBoundsChanged={onGroundBoundsChanged}/>
-          {showObstacle ? <Obstacles opacity={opacity} position={obstaclePosition}/> : null}
+          <Obstacles opacity={opacity} groundBounds={groundBounds}/>
           <ControlPanel opacity={opacity} onButtonClicked={onButtonClicked}/>
         </Physics>
       </Suspense>
